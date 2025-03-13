2645b885f472b1792002a63f6a3dc11114e34a7f
timothychemweno254 <timothy.kiprop@student.moringaschool.com>
2025-03-13 14:08:16 +0300

    find arrays

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+let records = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+];
+
+let found = records.find(function(record) {
+    if (record.year === "2015" && record.result === "W") {
+        return true;
+    } else if (record.year === "2014" && record.result === "N/A") {
+        return true;
+    } else if (record.year === "2013" && record.result === "L") {
+        return true;
+    } else {
+        return false;
+    }
+});
+
+function superbowlWin(records) {
+
+    const winningRecord = records.find(record => record.result === "W");
+
+        return winningRecord ? winningRecord.year : undefined;
+}
+
+console.log(found);
